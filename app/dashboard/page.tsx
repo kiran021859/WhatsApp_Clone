4605ccc2011a0dcf493be078267a468520c991a8
kiran021859 <kiranisaacs12@gmail.com>
2024-09-25 11:03:24 +0200

    routing complete

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function dashboard() {
+    return(
+    <div>
+        <h1>this is the dashboard</h1>
+    </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
       <h1>Hello</h1>
+      <button><Link href='/dashboard'>go to dashboard</Link></button>
     </div>
   );
 }
