d5c99f8d84c0e5c0e3f8d3605a348f24eef1698e
kiran021859 <kiranisaacs12@gmail.com>
2024-09-25 12:00:03 +0200

    fixed testing setup

--- __test__/index.test.tsx
+++ __test__/index.test.tsx
@@ -1,8 +1,24 @@
-import { expect, test } from 'vitest'
+import React from 'react'
+//import { expect, it, describe } from 'vitest'    add globals:true in vitest.config file and "types": ["vitest/globals"] to tsconfig file
 import { render, screen } from '@testing-library/react'
 import Page from '../app/page'
+import '@testing-library/jest-dom/vitest'
  
-test('Page', () => {
-  render(<Page />)
-  expect(screen.getByRole('heading', { level: 1, name: 'Hello' })).toBeDefined()
+describe('Page', () => {
+  it('page should say hello and button should render', ()=>{
+    render(<Page />)
+    const heading = screen.getByRole('heading')
+    expect(heading).toBeInTheDocument
+    expect(heading).toHaveTextContent(/Hello/i)
+
+    const button = screen.getByRole('button')
+    expect(button).toBeInTheDocument
+    expect(button).toHaveTextContent(/dashboard/i)
+  })
+
+  
+  
 })
+
+
+
